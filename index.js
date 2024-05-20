97b76cb051dc5c91615ac1fc7281fcb9ee07ebd1
Ratan Hussain <mdratanmia7395@gmail.com>
2024-05-20 12:51:58 +0300

    update text area

--- index.js
+++ index.js
@@ -1,3 +1,5 @@
+/** @format */
+
 // selet all element here.....
 let aramex = document.querySelector('#aramex');
 let tamex = document.querySelector('#tamex');
@@ -5,142 +7,138 @@ let jonex = document.querySelector('#jonex');
 let postAge = document.querySelector('#postAge');
 let businessFlow = document.querySelector('#businessFlow');
 let dal = document.querySelector('#DAL');
-let dataTextarea = document.querySelector('#dataTextarea');
+let dataArea = document.querySelector('#dataTextarea');
+let dataTextarea = dataArea.trim();
+
 let linksContainer = document.getElementById('linksContainer');
 let allLinks = 'https://rashof.fastcoo-solutions.com/fm/Printpicklist3PL/';
 
 // style here
-function style1(){
-         linksContainer.innerHTML = 'Please enter a pickup Id.......!';
-        linksContainer.style.color = 'red';
-        dataTextarea.style.scale = '1.01';
-        linksContainer.style.fontSize = '18px';
-        linksContainer.style.transition = 'all .5s';
-        dataTextarea.style.outline = '5px solid red';
-        dataTextarea.style.color = 'red';
-        dataTextarea.innerHTML = 'Please fill me.';
-        dataTextarea.style.transition = 'all .5s';
+function style1() {
+	linksContainer.innerHTML = 'Please enter a pickup Id.......!';
+	linksContainer.style.color = 'red';
+	dataTextarea.style.scale = '1.01';
+	linksContainer.style.fontSize = '18px';
+	linksContainer.style.transition = 'all .5s';
+	dataTextarea.style.outline = '5px solid red';
+	dataTextarea.style.color = 'red';
+	dataTextarea.innerHTML = 'Please fill me.';
+	dataTextarea.style.transition = 'all .5s';
 
-        setTimeout(() => {
-            linksContainer.innerHTML = 'Link will show here...!';
-            linksContainer.style.color = 'black';
-            dataTextarea.style.scale = '1';
-            linksContainer.style.transition = 'all .5s';
-            dataTextarea.style.outline = '';
-            dataTextarea.style.color = '';
-            dataTextarea.style.transition = 'all .5s';
-            dataTextarea.innerHTML = '';
-
-        }, 1000);
+	setTimeout(() => {
+		linksContainer.innerHTML = 'Link will show here...!';
+		linksContainer.style.color = 'black';
+		dataTextarea.style.scale = '1';
+		linksContainer.style.transition = 'all .5s';
+		dataTextarea.style.outline = '';
+		dataTextarea.style.color = '';
+		dataTextarea.style.transition = 'all .5s';
+		dataTextarea.innerHTML = '';
+	}, 1000);
 }
 
 // pickup list bulk print here
-function makeLink(event){
-    if(dataTextarea.value === ''){
-        style1();
+function makeLink(event) {
+	if (dataTextarea.value === '') {
+		style1();
+	} else {
+		let url = allLinks + dataTextarea.value + '/' + event.target.value;
+		window.open(url, '_blank');
+	}
+}
 
-    }else{
-        let url = allLinks + dataTextarea.value + "/" + event.target.value;
-         window.open(url, '_blank');
-    }
-};
-
-aramex.addEventListener('click' , makeLink);
-tamex.addEventListener('click' , makeLink);
-jonex.addEventListener('click' , makeLink);
-postAge.addEventListener('click' , makeLink);
-businessFlow.addEventListener('click' , makeLink);
-dal.addEventListener('click' , makeLink);
-
+aramex.addEventListener('click', makeLink);
+tamex.addEventListener('click', makeLink);
+jonex.addEventListener('click', makeLink);
+postAge.addEventListener('click', makeLink);
+businessFlow.addEventListener('click', makeLink);
+dal.addEventListener('click', makeLink);
 
 // Generate bulk link for all courier
 document.getElementById('generateLinks').addEventListener('click', () => {
-    if(dataTextarea.value === ''){
-        style1();
-    }else{
-        let dataTextarea = document.getElementById('dataTextarea');
-    let linksContainer = document.getElementById('linksContainer');
+	if (dataTextarea.value === '') {
+		style1();
+	} else {
+		let dataTextarea = document.getElementById('dataTextarea');
+		let linksContainer = document.getElementById('linksContainer');
 
-    // Get data from textarea and split it into an array
-    let data = dataTextarea.value.split('\n').filter(Boolean);
+		// Get data from textarea and split it into an array
+		let data = dataTextarea.value.split('\n').filter(Boolean);
 
-    let first = "https://rashof.fastcoo-solutions.com/fm/assets/all_labels/";
-    let last = ".pdf";
+		let first = 'https://rashof.fastcoo-solutions.com/fm/assets/all_labels/';
+		let last = '.pdf';
 
-    // Clear previous links
-    linksContainer.innerHTML = '';
+		// Clear previous links
+		linksContainer.innerHTML = '';
 
-    // Generate and append new links
-    data.forEach((label) => {
-        let atag = document.createElement('a');
-        let tag = first + label + last;
-        atag.innerHTML = tag;
-        atag.href = tag;
-        atag.target = "_blank";
+		// Generate and append new links
+		data.forEach((label) => {
+			let atag = document.createElement('a');
+			let tag = first + label + last;
+			atag.innerHTML = tag;
+			atag.href = tag;
+			atag.target = '_blank';
 
-        // Append the link to the container
-        linksContainer.appendChild(atag);
-        linksContainer.appendChild(document.createElement('br')); // Optional: add line break
-    });
-    }
+			// Append the link to the container
+			linksContainer.appendChild(atag);
+			linksContainer.appendChild(document.createElement('br')); // Optional: add line break
+		});
+	}
 });
-
 
 // Generate bulk link for Dal only
 document.getElementById('generateLinksDal').addEventListener('click', () => {
-    if(dataTextarea.value === ''){
-        style1();
-    }else{
-        let dataTextarea = document.getElementById('dataTextarea');
-    let linksContainer = document.getElementById('linksContainer');
+	if (dataTextarea.value === '') {
+		style1();
+	} else {
+		let dataTextarea = document.getElementById('dataTextarea');
+		let linksContainer = document.getElementById('linksContainer');
 
-    // Get data from textarea and split it into an array
-    let data = dataTextarea.value.split('\n').filter(Boolean);
+		// Get data from textarea and split it into an array
+		let data = dataTextarea.value.split('\n').filter(Boolean);
 
-    let first = "https://rashof.fastcoo-solutions.com/fm/Printpicklist3PL_bulk/";
-    let last = "/22";
+		let first =
+			'https://rashof.fastcoo-solutions.com/fm/Printpicklist3PL_bulk/';
+		let last = '/22';
 
-    // Clear previous links
-    linksContainer.innerHTML = '';
+		// Clear previous links
+		linksContainer.innerHTML = '';
 
-    // Generate and append new links
-    data.forEach((label) => {
-        let atag = document.createElement('a');
-        let tag = first + label + last;
-        atag.innerHTML = tag;
-        atag.href = tag;
-        atag.target = "_blank";
+		// Generate and append new links
+		data.forEach((label) => {
+			let atag = document.createElement('a');
+			let tag = first + label + last;
+			atag.innerHTML = tag;
+			atag.href = tag;
+			atag.target = '_blank';
 
-        // Append the link to the container
-        linksContainer.appendChild(atag);
-        linksContainer.appendChild(document.createElement('br')); // Optional: add line break
-    });
-    }
-
+			// Append the link to the container
+			linksContainer.appendChild(atag);
+			linksContainer.appendChild(document.createElement('br')); // Optional: add line break
+		});
+	}
 });
-
 
 // additional option for work smart
 function orderNo(event) {
-    if(dataTextarea.value === ''){
-        style1();
-    }else{
-        var orderno = document.getElementById('dataTextarea').value;
-        var url = event + encodeURIComponent(orderno);
-        // window.location.href = url;
-        window.open(url, '_blank');
-    }
+	if (dataTextarea.value === '') {
+		style1();
+	} else {
+		var orderno = document.getElementById('dataTextarea').value;
+		var url = event + encodeURIComponent(orderno);
+		// window.location.href = url;
+		window.open(url, '_blank');
+	}
 }
 
-
 // for label print only
-function print(event , format) {
-    if(dataTextarea.value === ''){
-        style1();
-    }else{
-        var orderno = document.getElementById('dataTextarea').value;
-        var url = event + encodeURIComponent(orderno) + encodeURIComponent(format);
-        // window.location.href = url;
-        window.open(url, '_blank');
-    }
+function print(event, format) {
+	if (dataTextarea.value === '') {
+		style1();
+	} else {
+		var orderno = document.getElementById('dataTextarea').value;
+		var url = event + encodeURIComponent(orderno) + encodeURIComponent(format);
+		// window.location.href = url;
+		window.open(url, '_blank');
+	}
 }
